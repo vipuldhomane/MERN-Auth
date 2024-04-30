cd0d34ee92f5cd95cf9271abbabe8cbcd98d0277
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-30 18:33:11 +0530

    Signup page frontend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUP</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="UserName"
+          id="username"
+          className="bg-slate-100 p-3"
+        />
+        <input
+          type="text"
+          placeholder="UserName"
+          id="username"
+          className="bg-slate-100 p-3"
+        />
+        <input
+          type="text"
+          placeholder="UserName"
+          id="username"
+          className="bg-slate-100 p-3"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-95">
+          SignUp
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
