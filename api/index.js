9fb9570568977b41f777e40daab1debe3aa7bc0c
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-30 12:07:13 +0530

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
-
+//Run server on Port
 app.listen(3000, () => {
   console.log("Server Listening on port 3000");
 });
