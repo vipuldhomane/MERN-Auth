ed2cc55e0d271b1534e659b428a2ee993e873e0b
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-30 17:24:33 +0530

    custom Error Handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  //hash the password to store in DB
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
@@ -10,6 +12,8 @@ export const signup = async (req, res) => {
 
     res.status(201).json({ message: "User Created Successfully!", newUser });
   } catch (error) {
-    res.status(500).json(error.message);
+    // res.status(500).json(error.message);
+    // next(error);
+    next(errorHandler(401, "This is custom error"));
   }
 };
--- api/index.js
+++ api/index.js
@@ -33,3 +33,14 @@ app.get("/", (req, res) => {
 //Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
