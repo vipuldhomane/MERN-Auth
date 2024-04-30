ecfa542c1e49bfd3d6efe8ef2dc2d53048da7dcb
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-04-30 12:52:24 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -4,9 +4,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 //Connect to MongoDB
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected to MongoDB");
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 const app = express();
 //Run server on Port
