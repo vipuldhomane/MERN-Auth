fa7a98f9aa0ef7261d7eefd61559093271376ed7
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-05-31 16:56:07 +0530

    "Added file upload functionality to Profile page with Firebase storage integration"

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,92 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+
+    // Upload the file and metadata
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log(progress);
+
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(error);
+      },
+      () => {
+        // Handle successful uploads on complete
+
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // console.log("File available at", downloadURL);
+          setFormData({ ...formData, imageUrl: downloadURL });
+        });
+      }
+    );
+  };
+
   return (
     <div className="p-5 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-5 ">
+        {/* Take input of the image  */}
+        <input
+          type="file"
+          ref={fileRef}
+          className=" hidden"
+          accept="image/.*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+
         <img
           src={currentUser.profilePicture}
           alt="Profile"
           className="h-24 w-24 self-center rounded-full object-cover cursor-pointer"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-600">
+              Error Uploading Image (File size must be below 2MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span>{`Uploading : ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-600">Image uploaded successfully</span>
+          ) : (
+            " "
+          )}
+        </p>
         <input
           type="text"
           placeholder="UserName"
